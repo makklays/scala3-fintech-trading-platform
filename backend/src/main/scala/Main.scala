74aeacd214e9540986e3dae525443756ea3234d0
Alexander <alexander@makklays.com.ua>
2026-07-19 02:13:11 +0200

    19-07-2026 Add new Main scala file.

--- backend/src/main/scala/Main.scala
+++ backend/src/main/scala/Main.scala
@@ -2,34 +2,67 @@ import cats.effect.*
 import com.comcast.ip4s.*
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.middleware.CORS
-import org.http4s.HttpRoutes
-import org.http4s.dsl.io.*
-import org.http4s.circe.CirceEntityDecoder.*
-import io.circe.generic.auto.*
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
-case class Order(symbol: String, amount: Double, side: String)
+// Импортируем компоненты нашей гексагональной структуры
+import domain.models.User
+import domain.TradingEngine
+import adapters.db.InMemoryUserRepository
+import adapters.http.OrderRoutes
 
+/**
+ * Main Class for the Scala 3 backend application.
+ * Composition Root that initializes layers and boots the server.
+ *
+ * @author Alexander Kuziv <makklays@gmail.com>
+ * @company TechMatrix18
+ * @version 0.0.1
+ * @since 18.07.2026
+ */
 object Main extends IOApp.Simple:
-private val logger = Slf4jLogger.getLogger[IO]
+  private val logger = Slf4jLogger.getLogger[IO]
 
-val tradingRoutes: HttpRoutes[IO] = HttpRoutes.of[IO] {
-  case req @ POST -> Root / "api" / "orders" =>
+  val run: IO[Unit] =
     for
-      order <- req.as[Order]
-  _     <- logger.info(s"Получен ордер от React: $order")
-  resp  <- Ok(s"Ордер на ${order.symbol} успешно обработан бэкендом")
-  yield resp
-}
+      _ <- logger.info("Initializing In-Memory Database State...")
 
-val corsService = CORS.policy.withAllowOriginAll.apply(tradingRoutes)
+      // 1. Создаем демо-пользователя, наполняя структуру согласно новой схеме PostgreSQL
+      demoUser = User(
+        id = Some(1L),
+        username = "demo_user",
+        email = "demo@techmatrix18.com",
+        usdBalance = 50000.00, // Стартовые 50k USD
+        btcBalance = 1.25,     // Стартовые 1.25 BTC
+        password = "encrypted_password_here"
+      )
 
-val run: IO[Unit] =
-  EmberServerBuilder
-    .default[IO]
-    .withHost(ipv4"0.0.0.0")
-    .withPort(port"8080")
-    .withHttpApp(corsService.orNotFound)
-    .build
-    .use(_ => logger.info("Scala 3 Бэкенд успешно запущен на порту 8080!") >> IO.never)
+      // 2. Инициализируем атомарный контейнер состояния базы данных в памяти (Ref)
+      dbState <- Ref.of[IO, Map[String, User]](Map("demo_user" -> demoUser))
+
+      // 3. Собираем зависимости гексагональной архитектуры (Связывание слоев)
+      userRepository = InMemoryUserRepository(dbState)
+      tradingEngine  = TradingEngine(userRepository)
+      orderRoutes    = OrderRoutes(tradingEngine)
+      userRoutes     = UserRoutes(userRepository)
+
+      // 4. Оборачиваем наши HTTP-роуты в middleware для поддержки CORS (запросы от React)
+      corsService = CORS.policy.withAllowOriginAll.apply(orderRoutes.routes)
+
+      _ <- logger.info("Starting Http4s Ember Server with WebSockets...")
+
+      // 5. Запуск сервера с поддержкой WebSockets
+      _ <- EmberServerBuilder
+        .default[IO]
+        .withHost(ipv4"0.0.0.0")
+        .withPort(port"8080")
+        // Подключаем WebSocketBuilder на этапе инициализации HTTP-приложения
+        .withHttpWebSocketApp(wsb =>
+          val streamRoutes = StreamRoutes(wsb)
+          // Склеиваем все роуты вместе с помощью оператора <+>
+          val allRoutes = userRoutes.routes <+> orderRoutes.routes <+> streamRoutes.routes
+          CORS.policy.withAllowOriginAll.apply(allRoutes).orNotFound
+        )
+        .build
+        .use(_ => logger.info("Scala 3 Trading Platform is ONLINE!") >> IO.never)
+    yield ()
 
